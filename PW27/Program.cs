dee71045aad9772d52ae6f6be50855d713b73360
Kiesaro <freestylerspb@gmail.com>
2023-01-17 14:02:10 +0300

    Example 27

---
+++ PW27/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int summ (int number)
+{
+    int sum = 0;
+    int k = 0;
+    while (number > 0)
+    {
+        k= number % 10;
+        sum =sum + k;
+        number = number / 10;
+    }
+    return sum;
+}
+Console.Write("Введите число = ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ответ = ");
+Console.Write(summ(number));
