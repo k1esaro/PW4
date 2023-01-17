5c89fd6f5aff6c00d96bd9ab25af1f8fe97ad9a7
Kiesaro <freestylerspb@gmail.com>
2023-01-17 14:16:58 +0300

    Example 29

---
+++ PW29/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] nums = new int[8];
+Console.WriteLine("Введите значения массива");
+for (int i = 0 ; i < 8; i++)
+    {
+        nums[i] = Convert.ToInt32(Console.ReadLine());
+    }
+Console.WriteLine($"[{string.Join(", ", nums)}]");
+
