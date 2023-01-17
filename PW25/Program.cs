6f70874a74f445e334d06953357b8bd6c5a18307
Kiesaro <freestylerspb@gmail.com>
2023-01-17 13:43:44 +0300

    first commit

---
+++ PW25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число A = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B = ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a;
+if (b>0)
+{
+    for (int i = 1 ; i < b;i++ )
+    {
+        result = result * a; 
+    }
+    Console.Write("Отвеn : ");
+    Console.WriteLine(result); 
+}
+else
+{
+   Console.WriteLine("Ошибка"); 
+}
